457c13afcb9d1040569e299748a2c8a1393f6bf8
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-11-26 20:44:28 +0530

    added fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import Courses from "./components/Courses";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import { Courses, Footer, Header, Hero } from "./components";
 
 function App() {
   return (
